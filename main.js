bbe76899590fe62405cf8afe362f88d5e2fa4258
Irvin Duarte <irvinjlduarte2009@gmail.com>
2025-07-24 13:51:11 -0500

    Created a DB Loading and Unloading System

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ const fs = require("fs");
 const https = require("https");
 const path = require("path");
 const cli = require(path.join(__dirname, "scripts/cli_log.js"));
+const db_wrapper = require(path.join(__dirname, "scripts/db.js"))
 
 cli.clear();
-cli.log("Loading Configuration");
+cli.log("Loading HTTPS & Certificate Configuration");
 
 //Import Config
 const http_conf = require(path.join(__dirname, "./config/http.json"));
@@ -24,6 +25,8 @@ try{
 
 }
 */
+//init db
+db_wrapper.init_db()
 
 //Setup Server
 const app = express();
@@ -35,5 +38,5 @@ app.post("/mail",function(req,res){
 
 //Start Server
 app.listen(http_conf.port, function () {
-  cli.log(`HTTP Server Listening |Port:${http_conf.port}|`);
+  cli.yay(`HTTP Server Listening |Port:${http_conf.port}|`);
 });
--- scripts/db.js
+++ scripts/db.js
@@ -1,12 +1,26 @@
-const sql3 = require("sqlite3");
-const cli = require("./cli_log.js")
+const cli = require("./cli_log.js");
 const path = require("path");
-const db_config = require(path.join(__dirname,"../config/db.json"));
+const db_config = require(path.join(__dirname, "../config/db.json"));
+const fs = require("fs");
 
-const db = new sql3.Database(db_config.filename,function(err){
-    if(err != null) cli.err(err); else cli.log("Loaded Database")
-process.exit()
-});
 function init_db() {
-    db.run("CREATE TABLE users ( id INTEGER PRIMARY KEY, username TEXT, email TEXT, password TEXT, name TEXT, phone_number TEXT)")
+  cli.log(`Looking for Exisitng Database |Basename:${db_config.basename}|`);
+  const files = fs.readdirSync(path.join(__dirname, "../data"));
+  const db_files = files.filter((file) => {
+    return file.startsWith(db_config.basename) && file.endsWith(".odb");
+  });
+  if (db_files.length === 0) {
+    cli.warn("No Databse Found");
+    cli.log(`Creating New Databse |Basename:${db_config.basename}|`);
+    fs.writeFileSync(
+      path.join(__dirname, "../data/", `${db_config.basename}0.odb`),
+      ""
+    );
+  } else {
+    cli.yay(`Database Found |Basename:${db_config.basename}|`);
+  }
 }
+
+module.exports = {
+  init_db,
+};
